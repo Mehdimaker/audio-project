006a0c4f88e586d76a44dcc95858de7e0cc11040
Mehdi <mehdi.maizate@gmail.com>
2015-08-23 21:21:52 +0200

    add pages

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,18 @@
+class PageController < ApplicationController
+  def home
+  end
+  def bio
+  end
+  def photos
+  end
+  def music
+  end
+  def videos
+  end
+  def news
+  end
+  def tour
+  end
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  #get 'page/home'
+get '/' => 'page#home'
+get '/bio' => 'page#bio'
+get '/photos' => 'page#photos'
+get '/music' => 'page#music'
+get '/videos' => 'page#videos'
+get '/news' => 'page#news'
+get '/tour' => 'page#tour'
+get '/contact' => 'page#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
