e1f4a7a852ed2097d9451f9b6e31d440f78b67a6
Mehdi <mehdi.maizate@gmail.com>
2015-08-26 09:56:58 +0200

    add css

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   #get 'page/home'
-get '/' => 'page#home'
-get '/bio' => 'page#bio'
-get '/photos' => 'page#photos'
-get '/music' => 'page#music'
-get '/videos' => 'page#videos'
-get '/news' => 'page#news'
-get '/tour' => 'page#tour'
-get '/contact' => 'page#contact'
+get '/' => 'pages#home'
+get '/bio' => 'pages#bio'
+get '/photos' => 'pages#photos'
+get '/music' => 'pages#music'
+get '/videos' => 'pages#videos'
+get '/news' => 'pages#news'
+get '/tour' => 'pages#tour'
+get '/contact' => 'pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
